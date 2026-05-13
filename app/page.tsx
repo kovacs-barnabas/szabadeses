296667c67fe77563321b8a2996ac04e9c0b74d85
kovacs-barnabas <kovacs.barnabas@students.jedlik.eu>
2026-05-13 11:28:44 +0200

    fogalom

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,18 @@ export default function Főoldal() {
         <h1 className="text-6xl md:text-7xl font-extrabold text-amber-300 tracking-wide drop-shadow-lg">
           Szabadesés
         </h1>
+
+       <p className="mt-6 max-w-2xl text-gray-300 text-lg leading-relaxed">
+          A szabadesés olyan mozgás, amikor egy test csak a gravitáció hatásának van kitéve,
+          és más erők (például légellenállás) nem befolyásolják a mozgását.
+          Ilyenkor a test állandó gyorsulással esik lefelé, amely a gravitációs gyorsulás.
+        </p>
+
+        <p className="mt-4 text-gray-400 max-w-xl">
+          A Földön ez az érték átlagosan <span className="text-amber-300 font-semibold">9.81 m/s²</span>.
+        </p>
+
+
       </main>
     </>
   );
