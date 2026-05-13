ff05a38cbb3265669fdd7f098295394f837f7faf
korodi-oliver <korodi.oliver@students.jedlik.eu>
2026-05-13 10:48:40 +0200

    fooldal2

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,33 @@ import Link from "next/link"
 export default function Főoldal() {
 
 return (
-    <main className="min-h-screen bg-black text-white flex flex-col items-center pt-10 gap-6">
 
-    <h1 className="text-5xl text-white">
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center gap-6">
+
+    <h1 className="text-5xl text-amber-300">
       Szabadesés
     </h1>
 
     <Link href="/szabadeses"
-      className="px-6 py-3 bg-blue-600 rounded-lg text-white text-xl hover:bg-blue-700 transition-colors"
+      className="px-6 py-3  text-white text-xl hover:bg-blue-700 transition-colors rounded-xl"
       >
         Kiszámitása
       </Link>
 
-      
+      <Link href="/bolygok"
+      className="px-6 py-3  text-white text-xl hover:bg-blue-700 transition-colors rounded-xl"
+      >
+        Bolygók
+      </Link>
+
+      <Link href="/kepletek"
+      className="px-6 py-3  text-white text-xl hover:bg-blue-700 transition-colors rounded-xl"
+      >
+        Képletek
+      </Link>
+
+
     
-    
-
-
-
-
-
-
-
-
     </main>
   )
 }
